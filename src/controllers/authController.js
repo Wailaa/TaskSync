123c84ab3a60db2202b9909671a9fa1dd447ae9f
Wail Abu Ahmad <wail.abuahmad@code.berlin>
2025-01-31 16:09:01 +0100

    user login route and contorller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,16 +1,16 @@
 import { getDB } from "../config/db.js";
-import { compareHashed, creatHashPass } from "../utils/hassPass.js"; 
+import { compareHashed, creatHashPass } from "../utils/hassPass.js";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 export const register = async (req, res) => {
     try {
-        const {username , password, email } = req.body;
+        const { username, password, email } = req.body;
         const db = getDB();
         const existingUser = await db.collection('users').findOne({ email });
-        if (existingUser){
-            return res.status(400).json({message : 'Usr already exists'});
+        if (existingUser) {
+            return res.status(400).json({ message: 'Usr already exists' });
         }
 
         const hashedPass = await creatHashPass(password)
@@ -22,15 +22,34 @@ export const register = async (req, res) => {
         };
 
         await db.collection('users').insertOne(newUser)
-        res.status(201).json({ message: 'User registered successfully'});
-    }catch (error){
+        res.status(201).json({ message: 'User registered successfully' });
+    } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Server error' });
     }
 };
 
-export const login = async (req,res) => {
-    console.log("login route activated")
-    res.status(201).json({ message: 'User logged in successfully'});
+export const login = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        if (!username || !password) {
+            return res.status(400).json({ message: "Username and password are required" });
+        }
 
+        const db = getDB();
+        const user = await db.collection("users").findOne({ username });
+        if (!user || !confirmPassword) {
+            return res.status(401).json({ message: "Invalid email or password" });
+        }
+
+        const isPassValid = await compareHashed(password, user.password);
+        if (!isPassValid) {
+            return res.status(401).json({ message: "Invalid username or password" });
+        }
+
+        return res.status(201).json({ message: "User logged in successfully" });
+    } catch (error) {
+        console.error("Login error:", error);
+        res.status(500).json({ message: "Internal Server error" });
+    }
 };
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,5 +4,6 @@ import {register,login} from "../controllers/authController.js"
 const userRouter = express.Router();
 
 userRouter.post("/register",register);
+userRouter.post("/login",login);
 
 export {userRouter};
